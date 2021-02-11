cc0fb7c397619e6d1fab6c3b5e14c3b59f4e06a5
tacobayle <nicolas.bayle@gmail.com>
2021-02-11 20:12:07 +0000

    20210211

--- ansible_bootstrap_k8s.tf
+++ ansible_bootstrap_k8s.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "ansible_hosts_cluster_master" {
   count            = length(var.vmw.kubernetes.clusters)
   provisioner "local-exec" {
-    command = "echo '---' | tee hosts_cluster_${count.index} ; echo 'all:' | tee -a hosts_cluster_${count.index} ; echo '  children:' | -a tee hosts_cluster_${count.index}; echo '    master:' | -a tee hosts_cluster_${count.index}; echo '      hosts:' | -a tee hosts_cluster_${count.index} ; echo '        ${vsphere_virtual_machine.master[count.index].default_ip_address}:' | tee -a hosts_cluster_${count.index}"
+    command = "echo '---' | tee hosts_cluster_${count.index} ; echo 'all:' | tee -a hosts_cluster_${count.index} ; echo '  children:' | tee -a hosts_cluster_${count.index}; echo '    master:' | tee -a hosts_cluster_${count.index}; echo '      hosts:' | tee -a hosts_cluster_${count.index} ; echo '        ${vsphere_virtual_machine.master[count.index].default_ip_address}:' | tee -a hosts_cluster_${count.index}"
   }
 }
 
@@ -9,7 +9,7 @@ resource "null_resource" "ansible_hosts_cluster_static1" {
   depends_on = [null_resource.ansible_hosts_cluster_master]
   count            = length(var.vmw.kubernetes.clusters)
   provisioner "local-exec" {
-    command = "echo '    workers:' | -a tee hosts_cluster_${count.index} ; echo '      hosts:' | -a tee hosts_cluster_${count.index}"
+    command = "echo '    workers:' | tee -a hosts_cluster_${count.index} ; echo '      hosts:' | tee -a hosts_cluster_${count.index}"
   }
 }
 
@@ -25,6 +25,6 @@ resource "null_resource" "ansible_hosts_cluster_static2" {
   depends_on = [null_resource.ansible_hosts_cluster_workers]
   count            = length(var.vmw.kubernetes.clusters)
   provisioner "local-exec" {
-    command = "echo '  vars:' | -a tee hosts_cluster_${count.index} ; echo '    ansible_user: ubuntu' | -a tee hosts_cluster_${count.index}; echo '    ansible_ssh_common_args: ${var.jump.private_key_path}' | tee -a hosts_cluster_${count.index}"
+    command = "echo '  vars:' | -a tee hosts_cluster_${count.index} ; echo '    ansible_user: ubuntu' | tee -a hosts_cluster_${count.index}; echo '    ansible_ssh_common_args: ${var.jump.private_key_path}' | tee -a hosts_cluster_${count.index}"
   }
 }
