505868dea5ddd6ba2a8db6f1b5195ae8b19753c0
tacobayle <nicolas.bayle@gmail.com>
2021-02-11 19:52:50 +0000

    20210211

--- ansible_bootstrap_k8s.tf
+++ ansible_bootstrap_k8s.tf
@@ -1,7 +1,12 @@
 resource "null_resource" "ansible_hosts_cluster_master" {
   count            = length(var.vmw.kubernetes.clusters)
   provisioner "local-exec" {
-    command = "tee hosts_cluster_${count.index} > /dev/null <<EOT\n---\nall:\n  children:\n    master:\n      hosts:\n        ${vsphere_virtual_machine.master[count.index].default_ip_address}:\nEOT"
+    command = "echo '---' | tee hosts_cluster_${count.index}",
+    command = "echo 'all:' | tee -a hosts_cluster_${count.index}",
+    command = "echo '  children:' | -a tee hosts_cluster_${count.index}",
+    command = "echo '    master:' | -a tee hosts_cluster_${count.index}",
+    command = "echo '      hosts:' | -a tee hosts_cluster_${count.index}",
+    command = "echo '        ${vsphere_virtual_machine.master[count.index].default_ip_address}:' | tee -a hosts_cluster_${count.index}"
   }
 }
 
@@ -9,7 +14,8 @@ resource "null_resource" "ansible_hosts_cluster_static1" {
   depends_on = [null_resource.ansible_hosts_cluster_master]
   count            = length(var.vmw.kubernetes.clusters)
   provisioner "local-exec" {
-    command = "tee -a hosts_cluster_${count.index} > /dev/null <<EOT\n    workers:\n      hosts:\nEOT"
+    command = "echo '    workers:' | -a tee hosts_cluster_${count.index}",
+    command = "echo '      hosts:' | -a tee hosts_cluster_${count.index}",
   }
 }
 
@@ -17,7 +23,7 @@ resource "null_resource" "ansible_hosts_cluster_workers" {
   depends_on = [null_resource.ansible_hosts_cluster_static1]
   count            = length(var.vmw.kubernetes.clusters) * var.vmw.kubernetes.workers.count
   provisioner "local-exec" {
-    command = "tee -a hosts_cluster_${floor(count.index / var.vmw.kubernetes.workers.count)} > /dev/null <<EOT        ${vsphere_virtual_machine.worker[count.index].default_ip_address}:\nEOT"
+    command = "echo '        ${vsphere_virtual_machine.worker[count.index].default_ip_address}:' | tee -a hosts_cluster_${floor(count.index / var.vmw.kubernetes.workers.count)}"
   }
 }
 
@@ -25,6 +31,8 @@ resource "null_resource" "ansible_hosts_cluster_static2" {
   depends_on = [null_resource.ansible_hosts_cluster_workers]
   count            = length(var.vmw.kubernetes.clusters)
   provisioner "local-exec" {
-    command = "tee -a hosts_cluster_${count.index} > /dev/null <<EOT  vars\n    ansible_user:  ubuntu\n    ansible_ssh_private_key_file: ${var.jump.private_key_path} \n    ansible_ssh_common_args: '-o StrictHostKeyChecking=no'\nEOT"
+    command = "echo '  vars:' | -a tee hosts_cluster_${count.index}",
+    command = "echo '    ansible_user: ubuntu' | -a tee hosts_cluster_${count.index}",
+    command = "echo '    ansible_ssh_common_args: ${var.jump.private_key_path}' | tee -a hosts_cluster_${count.index}"
   }
 }
